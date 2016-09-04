f368e0f2d33d6f4da51f264230c5726665afaf44
momothereal <momo@momoperes.ca>
2016-09-04 00:25:27 -0400

    Changes to the smoothing logic

---
+++ js/general.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $("a").on('click', function(event) {
+    if (this.hash !== "" && $(this).attr("href").startsWith("#")) {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
