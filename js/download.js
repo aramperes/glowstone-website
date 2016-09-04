15e484cb58d5056461906a7f4589fc47084c3a7d
momothereal <momo@momoperes.ca>
2016-09-04 18:01:31 -0400

    Update downloads section

--- js/download.js
+++ js/download.js
@@ -1,10 +1,20 @@
+
+function download(url) {
+  console.log("Hello");
+  if ($("#download-warning").is(":hidden")) {
+    $("#download-warning").slideDown(500);
+    window.open(url);
+  } else {
+    $("#download-warning").slideUp(500);
+  }
+}
+
+
 $(function() {
   $("#download-latest").click(function() {
-    if ($("#download-warning").is(":hidden")) {
-      $("#download-warning").slideDown(500);
-      window.location.href = "https://bamboo.gserv.me/browse/GSPP-SRV/latest/artifact/shared/Version-Independent-Server-JAR/glowstone.jar";
-    } else {
-      $("#download-warning").slideUp(500);
-    }
+    download("https://bamboo.gserv.me/browse/GSPP-SRV/latest/artifact/shared/Version-Independent-Server-JAR/glowstone.jar");
+  });
+  $("#download-snapshot").click(function() {
+    download("https://204-33457443-gh.circle-artifacts.com/0//tmp/circle-artifacts.qebqzFq/glowstone++-1.11-16w35a-SNAPSHOT.jar");
   });
 });
