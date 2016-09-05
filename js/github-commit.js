3eb2e3d0d576488785929a30decbc09b078ade8b
momothereal <momo@momoperes.ca>
2016-09-04 21:04:52 -0400

    Add instant click, modify commit message a little

--- js/github-commit.js
+++ js/github-commit.js
@@ -6,7 +6,7 @@ $(function() {
     var message = commit.message;
     var link = "https://github.com/GlowstoneMC/Glowstone/commit/" + sha;
     $("#commit-parent").attr("href", link);
-    $("#commit-details").html('<span class="fa fa-github"></span>&nbsp;&nbsp; Latest commit:&nbsp;<strong>' + author + '</strong>&nbsp;pushed commit&nbsp;<i>' + sha.substr(0, 7) + '</i>: ' + message);
+    $("#commit-details").html('<span class="fa fa-github"></span>&nbsp;&nbsp; Latest commit:&nbsp;<code>' + sha.substr(0, 7) + '</code> by <strong>' + author + '</strong>: ' + message);
   }).error(function(err){
     $("#commit-details").html('<span class="fa fa-warning"></span> &nbsp; Unable to fetch latest commit.');
   });
